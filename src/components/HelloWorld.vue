3aee6c106c3992e770d98b0c559a1f375b123bb4
mario <fraire.tepox.mario.valentin@gmail.com>
2024-10-29 20:43:34 -0600

    tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,7 @@
 <template>
   <h1>Hello World</h1>
-  <p>pagina de test</p>
+  <p class="text-3xl">pagina de test</p>
+  <p class="text-3xl">pagina de test</p>
+  <p class="text-3xl">pagina de test</p>
+  <p class="text-3xl">pagina de test</p>
 </template>
