c3c9ff67930df2b9e5efa487db49151e5661f274
Valeria <valeriahernandezgutierrezluzac@gmail.com>
2024-11-06 20:17:42 -0600

    adición de clases de colores de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        black: '#000000',
+        rosy : '#f7f7f7',
+        yellow: '#ffd182',
+        magenta: '#970046',
+        red: '#db3639'
+      },
+    },
   },
   plugins: [],
 }
