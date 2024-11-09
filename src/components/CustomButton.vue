cb2f804404b4a56bf1d1e4cef54ea2bb55e12ee6
Valeria <valeriahernandezgutierrezluzac@gmail.com>
2024-11-09 14:23:35 -0600

    creación de componente de botón personalizado, y de archivo del item de búsqueda

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,25 @@
+<template>
+        <button :class="[`bg-${props.color}`,'hover:shadow-lg', `hover:shadow-${props.color}/50`, 'hover:scale-105' ]"><slot></slot></button>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue'
+const props = defineProps({
+    color: String
+})
+</script>
+
+<style scoped>
+button{
+    @apply rounded-lg;
+    @apply font-made-tommy;
+    @apply font-light;
+    @apply text-lg;
+    @apply text-white;
+    @apply p-2;
+    @apply w-56;
+    @apply transition;
+    @apply duration-500;
+}
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,19 @@
 <template >
   <h1>Hello World</h1>
-  <div class="bg-black h-screen flex justify-center p-4 gap-12">
+  <div class="bg-black h-screen flex flex-wrap justify-center p-4 gap-12">
     <HomeEventItem v-for="item in arr" :key="item" :eventName="item.eventName" :eventType="item.eventType" :img="item.img"></HomeEventItem>
+    <div class=" p-6">
+      <CustomButton color="red">Hola</CustomButton>
+    </div>
   </div>
-  <p class="text-3xl">pagina de test</p>
+
   <p class="text-3xl">pagina de test</p>
   <p class="text-3xl">pagina de test</p>
   <p class="text-3xl">pagina de test</p>
 </template>
 
 <script setup>
+import CustomButton from './CustomButton.vue';
 import HomeEventItem from './HomeEventItem.vue';
 const arr = [
   {img: "https://i.pinimg.com/736x/de/17/5b/de175b2222cc1981b68fc3774c157d72.jpg", eventName: "The Eras Tour", eventType: "Concierto"},
