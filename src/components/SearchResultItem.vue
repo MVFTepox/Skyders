5fd16572a216103cd84a2c069a22de3ed7663478
Valeria <valeriahernandezgutierrezluzac@gmail.com>
2024-11-11 20:35:27 -0600

    edicion de responsividad a resultado de búsqueda

--- src/components/SearchResultItem.vue
+++ src/components/SearchResultItem.vue
@@ -2,15 +2,15 @@
     <div class="w-full flex items-center h-48 p-4 px-8 gap-6 sm:gap-12 border-b-[1px] border-white cursor-pointer font-made-tommy">
         <img :src="props.img" alt="" class="w-[15%] h-full object-cover object-center">
         <div class="flex flex-col gap-4">
-            <p class="text-yellow text-md">{{ props.eventType }}</p>
-            <h3 class="font-bold text-2xl text-white">{{ props.eventName }}</h3>
+            <p class="text-yellow text-sm md:text-base">{{ props.eventType }}</p>
+            <h3 class="font-bold text-xl sm:text-2xl text-white">{{ props.eventName }}</h3>
         </div>
-        <div class="flex flex-col text-xl  items-center text-white">
+        <div class="flex flex-col text-lg md:text-xl  items-center text-white">
             <p>{{ props.month }}</p>
-            <p class="font-bold text-2xl">{{ props.day }}</p>
+            <p class="font-bold text-xl sm:text-2xl">{{ props.day }}</p>
             <p>{{ props.year }}</p>
         </div>
-        <div class="flex flex-col gap-4 text-md">
+        <div class="flex flex-col gap-4 text-sm  md:text-base">
             <p class="text-yellow">{{props.dayOfWeek}} • {{ props.time }} </p>
             <p class="font-bold text-white">{{ props.state}} • {{ props.venue }}</p>
         </div>
