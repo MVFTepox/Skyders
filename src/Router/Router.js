b7d7f522cc57063e8e09aa41e4548a50d3b9ff64
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-11-28 10:30:37 -0600

    Update Router.js

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,6 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+
+
+
+
+
+        
         {path: '/MOMENTO', component: () => import('../components/Ev-Info.vue')},
         {path: '/QRCD', component: () => import('../components/QRCode.vue')},
         {path: '/BRCD', component: () => import('../components/CodigoBarras.vue')}
